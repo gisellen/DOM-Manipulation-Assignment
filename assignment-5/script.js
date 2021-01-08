e21bd0245c5745b63f8e687f0306365959815d84
Gisellen <giselle.domingo.x@gmail.com>
2021-01-08 18:00:27 -0500

    added function to select color and fill a cell

--- assignment-5/script.js
+++ assignment-5/script.js
@@ -11,3 +11,15 @@ $("#delete").click(function() {
     const lastchild = document.querySelector(".container").lastElementChild
     lastchild.remove();
 });
+
+//get colors
+let red = document.getElementById("colorSelect").value
+console.log(red)
+
+//event listener for each cell without jquery
+const cell = document.querySelectorAll(".cell");
+cell.forEach(block => block.addEventListener("click", block => {
+  const element = block.srcElement
+  let color = document.getElementById("colorSelect").value
+  element.style.backgroundColor = color
+}))
