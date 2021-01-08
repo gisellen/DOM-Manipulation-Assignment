a658ff0c3d0437d88df02bba599b6be8be63da59
Gisellen <giselle.domingo.x@gmail.com>
2021-01-08 18:17:59 -0500

    Added function to fill/clear all

--- assignment-5/script.js
+++ assignment-5/script.js
@@ -1,25 +1,49 @@
+//script makes use of jquery & DOM API
+
+
 //add cell
-$("#add").click(function() {
+$("#add").click(() => {
   const container = document.createElement("div");
   container.innerText = "O";
   $(".container").append(container);
   container.setAttribute("class", "cell");
+  const cell = document.querySelector(".container").lastElementChild
+  cell.addEventListener("click", onClickCell(cell))
+
 });
 
 //delete cell
-$("#delete").click(function() {
+$("#delete").click(() => {
     const lastchild = document.querySelector(".container").lastElementChild
     lastchild.remove();
 });
 
-//get colors
-let red = document.getElementById("colorSelect").value
-console.log(red)
-
-//event listener for each cell without jquery
-const cell = document.querySelectorAll(".cell");
-cell.forEach(block => block.addEventListener("click", block => {
-  const element = block.srcElement
+//fill all
+$("#fillAll").click(() => {
+  const cell = document.querySelectorAll(".cell");
   let color = document.getElementById("colorSelect").value
-  element.style.backgroundColor = color
-}))
+  cell.forEach(block => {
+    block.style.backgroundColor = color
+  })
+})
+
+//clear all
+$("#clearAll").click(() => {
+  const cell = document.querySelectorAll(".cell");
+  cell.forEach(block => {
+    block.style.backgroundColor = "transparent"
+  })
+})
+
+//event listener for each cell
+const cell = document.querySelectorAll(".cell");
+cell.forEach(block => onClickCell(block))
+
+//function to add event listener
+function onClickCell(block){
+  block.addEventListener("click", block => {
+    const element = block.srcElement
+    let color = document.getElementById("colorSelect").value
+    element.style.backgroundColor = color
+  })
+}
