c91770cbdb26e1a6edd78c48be2801aca4d013a8
Gisellen <giselle.domingo.x@gmail.com>
2021-01-08 19:14:02 -0500

    user is able to click and hold to fill cells

--- assignment-5/script.js
+++ assignment-5/script.js
@@ -4,9 +4,8 @@
 //add cell
 $("#add").click(() => {
   const container = document.createElement("div");
-  container.innerText = "O";
   $(".container").append(container);
-  container.setAttribute("class", "cell");
+  container.setAttribute("class", "cell no-select");
   const cell = document.querySelector(".container").lastElementChild
   cell.addEventListener("click", onClickCell(cell))
 
@@ -40,10 +39,25 @@ const cell = document.querySelectorAll(".cell");
 cell.forEach(block => onClickCell(block))
 
 //function to add event listener
+let down =false;
+$(document).mousedown(function() {
+  down = true;
+}).mouseup(function() {
+  down = false;  
+});
+
 function onClickCell(block){
   block.addEventListener("click", block => {
     const element = block.srcElement
     let color = document.getElementById("colorSelect").value
     element.style.backgroundColor = color
+  })
+  block.addEventListener("mousemove", block=>{
+    if(down){
+      console.log(block)
+      const element = block.srcElement
+      let color = document.getElementById("colorSelect").value
+      element.style.backgroundColor = color
+    }
   })
 }
