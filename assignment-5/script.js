a3c9b07ab5d3ce14df69c28c9c668593a4948d56
Gisellen <giselle.domingo.x@gmail.com>
2021-01-08 20:22:16 -0500

    added the function to add/delete columns

--- assignment-5/script.js
+++ assignment-5/script.js
@@ -1,20 +1,36 @@
 //script makes use of jquery & DOM API
 
+//add column
+$("#addColumn").click(() => {
+  // const container = document.createElement("div");
+  // $(".container").append(container);
+  // container.setAttribute("class", "cell no-select");
+  // const cell = document.querySelector(".container").lastElementChild
 
-//add cell
-$("#add").click(() => {
-  const container = document.createElement("div");
-  $(".container").append(container);
-  container.setAttribute("class", "cell no-select");
-  const cell = document.querySelector(".container").lastElementChild
-  cell.addEventListener("click", onClickCell(cell))
+  const firstChild = document.querySelector(".container").firstElementChild.childElementCount
+  const container = document.querySelector(".container")
 
+
+  const col = document.createElement("div");
+  col.setAttribute("class", "col");
+  for (let i = 0; i < firstChild; i++) {
+    const cell = document.createElement("div");
+    cell.setAttribute("class", "cell no-select");
+    cell.addEventListener("click", onClickCell(cell))
+    col.append(cell);
+  }
+  columnCount = container.childElementCount
+  container.style.gridTemplateColumns = "repeat("+ (columnCount + 1)+", 1fr)"
+  container.append(col);
 });
 
-//delete cell
-$("#delete").click(() => {
-    const lastchild = document.querySelector(".container").lastElementChild
-    lastchild.remove();
+//delete column
+$("#deleteColumn").click(() => {
+  const container = document.querySelector(".container")
+  const lastchild = document.querySelector(".container").lastElementChild
+  lastchild.remove();
+  columnCount = container.childElementCount
+  container.style.gridTemplateColumns = "repeat("+ (columnCount)+", 1fr)"
 });
 
 //fill all
@@ -39,21 +55,21 @@ const cell = document.querySelectorAll(".cell");
 cell.forEach(block => onClickCell(block))
 
 //function to add event listener
-let down =false;
-$(document).mousedown(function() {
+let down = false;
+$(document).mousedown(function () {
   down = true;
-}).mouseup(function() {
-  down = false;  
+}).mouseup(function () {
+  down = false;
 });
 
-function onClickCell(block){
+function onClickCell(block) {
   block.addEventListener("click", block => {
     const element = block.srcElement
     let color = document.getElementById("colorSelect").value
     element.style.backgroundColor = color
   })
-  block.addEventListener("mousemove", block=>{
-    if(down){
+  block.addEventListener("mousemove", block => {
+    if (down) {
       console.log(block)
       const element = block.srcElement
       let color = document.getElementById("colorSelect").value
